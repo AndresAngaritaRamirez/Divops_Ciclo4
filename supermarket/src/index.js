90c46a8666a9b125d6399e0798a0313002e6378f
Carlos Reyes <cjpiraligua29@gmail.com>
2022-10-25 23:03:29 -0500

    header index

--- supermarket/src/index.js
+++ supermarket/src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 import * as bootstrap from 'bootstrap';
+import './Styles/Estilos.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(<>
+    <header>
+        <div className="wrapper">
+            <div className="logo">Logo de divops</div>
+            <nav>
+                <a href="#">Inicio</a>
+                <a href="#">Recargar</a>
+            </nav>
+        </div>
+    </header>
     <div className='container'>
         <div className='card'>
             <div className='card-header'>
