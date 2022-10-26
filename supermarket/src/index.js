99c60a5dd8516c560f0dc168aa595325f06a3d12
Ivan Silva <isilvao@unal.edu.co>
2022-10-25 21:49:53 -0500

    Primera actualizacion

--- supermarket/src/index.js
+++ supermarket/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<>
+    <div className='container'>
+        <div className='card'>
+            <div className='card-header'>
+                <h1>React Bootstrap</h1>
+            </div>
+            <div className='card-body'>
+                <p>Bootstrap is a popular CSS framework.</p>
+            </div>
+        </div>
+    </div>
+</>
+)
