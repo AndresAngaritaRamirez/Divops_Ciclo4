c64e9f5e17e5b2ef97ce6eb08ec3ccf24d314f2b
Ivan Silva <isilvao@unal.edu.co>
2022-10-26 20:27:44 -0500

    Cambios de la pagina

--- supermarket/src/index.js
+++ supermarket/src/index.js
@@ -2,33 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Principal } from './components/Principal';
+import { Header } from './components/Header';
 import * as bootstrap from 'bootstrap';
 import './styles/navs.css'
+import './styles/estilos.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(<>
+    <Header />
     <div className='container navs'>
             <Principal />
-        <header>
-            <div className="wrapper">
-                <div className="logo">Logo de divops</div>
-                <nav>
-                    <a href="#">Inicio</a>
-                    <a href="#">Recargar</a>
-                </nav>
-            </div>
-        </header>
-        <div className='container'>
-            <div className='card'>
-                <div className='card-header'>
-                    <h1>React Bootstrap</h1>
-                </div>
-                <div className='card-body'>
-                    <p>Bootstrap is a popular CSS framework.</p>
-                </div>
-            </div>
-        </div>
     </div>
 </>
 )
